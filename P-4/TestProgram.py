7da491bedad9c0801db4d86ff6dbf6ed7534d7e4
OJH0301 <steaksteak@naver.com>
2024-11-05 23:17:07 +0900

    11

--- P-4/TestProgram.py
+++ P-4/TestProgram.py
@@ -3,9 +3,9 @@ from CircularQueue import CircularQueue
 Queue = CircularQueue(10)
 
 while True :
-    command = input("[메뉴선택] i-입력, d-삭제=> ")
+    command = input("[메뉴선택] e-입력, d-삭제=> ")
 
-    if command == 'i' :
+    if command == 'e' :
         str = input("  입력할 값: ")
         Queue.enqueue(str)
         print(Queue)
